70af2cff1805940635352273824979b9b278568f
leeiv <44705038+leeiv@users.noreply.github.com>
2018-12-03 09:43:04 -0700

    Update TestMotionCollision.py

--- TestMotionCollision.py
+++ TestMotionCollision.py
@@ -1,222 +1,212 @@
-import re, operator
+# Starter code for an adventure type game.
+# University of Utah, David Johnson, 2017.
+# This code, or code derived from this code, may not be shared without permission.
+# Authors: Ivan Lee and Celine Cavanaugh
 
-# Initial code written by David Johnson, University of Utah.
-# Author of final code: Celine Cavanaugh, Ivan Lee
-
-
-#
-#
-# This section of code is fully implemented and provided for you to use.
-#
-#
-
-# This function is called with a dictionary. It will return
-# a list of tuples in the form (key, value) and ordered by
-# the value in value. So given a
-# dict = {"c":12, "a":2, "b":1}
-# it will return a list
-# [("b",1), ("a",2), ("c",12)]
-def make_sorted_list_by_value_from_dictionary(dict):
-    return sorted(dict.items(), key=operator.itemgetter(1))
-
-# This function removes everything except the letters a-z from words.
-# So it removes punctuation, unusual accented letters, etc.
-def remove_punctuation(words):
-    clean_words = []
-    for word in words:
-        clean_word = re.sub("[^a-z ]+", "", word)
-        if len(clean_word) > 0:
-            clean_words.append(clean_word)
-    return clean_words
-
-# This function reads the 'stop_words.txt' file and returns each word from the file in a list.
-def read_common_words():
-    stopWords = []
-    with open('stop_words.txt','r') as file:
-        for word in file:
-            word = word.strip() # There are some hidden characters in the file that need to be removed
-            stopWords.append(word)
-    return stopWords
-
-# Given a list of words, break the list into chunks of length chunk_size (the last chunk
-# is just whatever is left over). Create a list of these chunks and return that.
-# Given a list
-# ['a', 'boy', 'is', 'a', 'very', 'good', 'boy']
-# and a chunk_size of 2, this would return a list of chunks of length 2
-# [['a', 'boy'], ['is', 'a'], ['very', 'good'], ['boy']]
-# This function is implemented for you.
-def break_words_into_chunks(text_words, chunk_size):
-    chunk_list = []
-    for index in range(0, len(text_words), chunk_size):
-        chunk_list.append(text_words[index:index+chunk_size])
-    return chunk_list
-
-#
-#
-# This section of code needs to be implemented by you.
-#
-#
+import sys, pygame, math
 
 
-# Given a filename, open that file, read all the lines and return a list of all the words in the file.
-# Use split() to break a line into words.
-def read_all_words_in_file(filename):
+# This function loads a series of sprite images stored in a folder with a
+# consistent naming pattern: sprite_# or sprite_##. It returns a list of the images.
+def load_piskell_sprite(sprite_folder_name, number_of_frames):
+    frame_counts = []
+    padding = math.ceil(math.log(number_of_frames, 2))
+    for frame in range(number_of_frames):
+        folder_and_file_name = sprite_folder_name + "/sprite_" + str(frame).rjust(padding, '0') + ".png"
+        frame_counts.append(pygame.image.load(folder_and_file_name).convert_alpha())
 
-    words = list()
-
-    with open (filename) as file:
-        for line in file:
-            sentenceWords = line.split()
-            for word in sentenceWords:
-                words.append(word)
-    return words
+    return frame_counts
 
 
-
-# Given a list of words, return a new list with all the words lower case. Use
-# the string lower() method to make the word lower case.
-def make_lowercase(words):
-    newlist = list()
-    for word in words:
-        newWord = str(word).lower()
-        newlist.append((newWord))
-    return newlist
-
-# This function makes a new list from book_words that has all the words in
-# common_words removed. Go through the words in book_words and add it to the
-# new list if the word isn't in the common_words list. Return the new list.
-def remove_common_words(book_words, common_words):
-    newlist = list()
-    for word in book_words:
-        if not common_words.__contains__(word):
-            newlist.append(word)
-    return newlist
-
-# Given a list of text_words and another list of words, count how many times
-# any word in text_words has a match in other_list.
-# For
-# text_words = ['a', 'the', 'a', 'boy']
-# and
-# other_list = ['a', 'the']
-# this function would return 3, since 'a' appears 2 times, 'the' once, and 'boy'
-# no times.
-# Recall that the "in" operator is a good way of telling if a value is in a list.
-def count_number_of_words_that_appear_in_another_list(text_words, other_list):
-    word_count = 0
-    for word in text_words:
-        if other_list.__contains__(word):
-            word_count += 1
-    return word_count
-
-# Calculate the average length of all the words in text_words.
-# Add up the length of each word and divide by the number of words.
-# Return that average.
-def average_word_length(text_words):
-    total_words = 0
-    total_word_length = 0
-    for word in text_words:
-        total_words += 1
-        total_word_length = total_word_length + len(word)
-        average_length = total_word_length/total_words
-        #I'm not sure if this works
-    return average_length # Need to implement this function
-
-# Make a dictionary that has words of text_words as keys and how many
-# times that word appears in text_words as the value.
-# Refer to the lecture slides for how to do this.
-# Return the dictionary.
-def calculate_word_frequency(text_words):
-    word_frequency = {}
-    for word in text_words:
-        if word in word_frequency:
-            word_frequency[word] += 1
-        else:
-            word_frequency[word] = 1
-    return word_frequency
-
-# Count the words in word_list that appear in positive_words, then count the
-# words in word_list that appear in negative words.
-# Turn each count into a percentage by dividing by the length of word_list and then
-# multiply by 100.
-# Return the difference between the positive percentage and the negative percentage rounded
-# to 2 decimal places. The code for rounding is provided.
-# A positive returned number means there were more positive than negative words.
-# As an example, if
-# word_list = ['a', 'sad', 'boy', 'is', 'an', 'excellent', 'good', 'boy']
-# positive_words = ['excellent', 'good']
-# negative_words = ['sad']
-# The positive count would be 2 (for excellent and good)
-# The negative count would be 1 (for sad)
-# The positive percentage would be 2/8 * 100 = 25
-# the negative percentage would be 1/8 * 100 = 12.5
-# The returned value would be 25 - 12.5 = 12.50.
-# You should use the count_number_of_words_that_appear_in_another_list function to do the counting.
-def calculate_sentiment(word_list, positive_words, negative_words):
-    total_word = 0
-    positive_amount = 0
-    negative_amount = 0
-    for word in word_list:
-        total_word += 1
-        if positive_words.__contains__(word):
-            positive_amount += 1
-        if negative_words.__contains__(word):
-            negative_amount += 1
-    positive_percentage = positive_amount / total_word * 100
-    negative_percentage = negative_amount / total_word * 100
-    difference = positive_percentage - negative_percentage
-    return round(difference,2) # Implement and replace 0.0 with the difference between the positive and negative percents.
-
-
-# This code runs when the file is executed and is fully implemented.
+# The main loop handles most of the game
 def main():
+    # Initialize pygame
+    pygame.init()
 
-    # Read in the positive and negative words for later use.
-    positive_words = read_all_words_in_file("positive.txt")
-    negative_words = read_all_words_in_file("negative.txt")
+    screen_size = height, width = (700, 500)
+    screen = pygame.display.set_mode(screen_size)
 
-    # Analyze the following files. You may want to comment out all files but 1 when testing.
-    files = ["HuckFinn.txt", "TheScarletLetter.txt", "Plato.txt", "TaleOfTwoCities.txt"]
+    # create the hero character
+    hero = load_piskell_sprite("hero", 1)
+    hero_rect = hero[0].get_rect()
+    hero_rect.center = (350, 250)
 
-    # Loop over all the files
-    for file in files:
-        # Report on which file is being analyzed
-        print("\n\nAnalyzing:", file)
+    # add in another character
+    snake = pygame.image.load("snake.png").convert_alpha()
+    snake_rect = snake.get_rect()
+    snake_rect.center = 700, 800
 
-        # get the words from the text and clean them up
-        print("Getting words from book")
-        book_words = read_all_words_in_file(file)
-        book_words = make_lowercase(book_words)
-        #book_words = remove_punctuation(book_words)
-        print("    ", len(book_words), " words found")
+    sword = pygame.image.load("sword.png").convert_alpha()
+    sword_rect = sword.get_rect()
+    sword_rect.center = 200, 200
 
-        # Remove common words
-        common_words = read_common_words()
-        print("Removing common words")
-        book_words = remove_common_words(book_words, common_words)
-        print("    ", len(book_words), " words remaining")
+    potion = pygame.image.load("potion.png").convert_alpha()
+    potion_rect = potion.get_rect()
+    potion_rect.center = 500, 300
 
-        # Break the list of words into 20 chunks
-        chunks = break_words_into_chunks(book_words, len(book_words)//20 + 1)
+    crown = pygame.image.load("crown.png").convert_alpha()
+    crown_rect = crown.get_rect()
+    crown_rect.center = 100, 200
 
-        # Look at overall and chunk sentiment
-        print("Calculating Sentiment")
-        print("    Overall:", calculate_sentiment(book_words, positive_words, negative_words))
-        print("    Chunk Sentiment: ", end='')
-        for chunk in chunks:
-            print(calculate_sentiment(chunk, positive_words, negative_words), end=' ')
-        print()
+    key = pygame.image.load("key.png").convert_alpha()
+    key_rect = key.get_rect()
+    key_rect.center = 100, 200
 
-        # Find the average word length
-        print("Average word length:", round(average_word_length(book_words),1))
+    # add in a treasure item
+    treasure = pygame.image.load("treasurechest2.png").convert_alpha()
+    treasure_rect = treasure.get_rect()
+    treasure_rect.center = 750, 400
 
-        # Count up the most common words and report on the top 10.
-        print("Counting most common words")
-        word_frequencies = calculate_word_frequency(book_words)
-        word_frequency_list = make_sorted_list_by_value_from_dictionary(word_frequencies)
-        word_frequency_list.reverse()
-        print("    Most common words:", word_frequency_list[:10])
+    # The clock helps us manage the frames per second of the animation
+    clock = pygame.time.Clock()
+
+    # Mostly used to cycle the animation of sprites
+    frame_count = 0;
+
+    # variable to show if we are still playing the game
+    playing = True
+
+    # variable for hero direction
+    is_facing_left = True
+
+    # Variable to track text on the screen. If you set the dialog string to something and set the position and the
+    # counter, the text will show on the screen for dialog_counter number of frames.
+    dialog_counter = 0
+    dialog = ''
+    dialog_position = (0, 0)
+
+    # Load font
+    pygame.font.init()  # you have to call this at the start,
+    # if you want to use this module.
+    myfont = pygame.font.SysFont('Comic Sans MS', 30)
+
+    # create the inventory and make it empty
+    inventory = {}
+
+    # This list should hold all the sprite rectangles that get shifted with a key press.
+    rect_list = [snake_rect, treasure_rect, key_rect, crown_rect, sword_rect, potion_rect]
+
+    # Loop while the player is still active
+    while playing:
+        # start the next frame
+        screen.fill((170, 190, 190))
+
+        # Check events by looping over the list of events
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                playing = False
+
+        # check for keys that are pressed
+        # Note the indent makes it part of the while playing but not part of the for event loop.
+        keys = pygame.key.get_pressed()
+        # check for specific keys
+        # movement says how the world should shift. Pressing keys changes the value in the movement variables.
+        movement_x = 0
+        movement_y = 0
+        if keys[pygame.K_LEFT]:
+            is_facing_left = True
+            movement_x += 5
+        if keys[pygame.K_RIGHT]:
+            is_facing_left = False
+            movement_x -= 5
+        if keys[pygame.K_UP]:
+            movement_y += 5
+        if keys[pygame.K_DOWN]:
+            movement_y -= 5
+
+        # Move all the sprites in the scene by movement amount.
+        # You can still move the rect of an individual sprite to make
+        # it move around the landscape.
+        for rect in rect_list:
+            rect.move_ip(movement_x, movement_y)
+
+        # Check for touching ghost.
+        if hero_rect.colliderect(snake_rect):
+            # Respond differently depending on gold status
+            if "sword" and "potion" and "crown" in inventory:
+                dialog = "No! You have defeated me."
+            else:
+                dialog = "Die puny mortal. (game end)"
+            # These say where and for how long the dialog prints on the screen
+            dialog_counter = 50
+            dialog_position = (100, 100)
 
 
-# This runs if the file is executed and doesn't if the file is imported
-if __name__ == '__main__':
-    main()
+        # Check for touching the gold chest.
+        if hero_rect.colliderect(treasure_rect) and "gold" not in inventory:
+            inventory["gold"] = True
+            dialog = "Gold added to inventory"
+            dialog_counter = 30
+            dialog_position = (300, 200)
+
+        if hero_rect.colliderect(sword_rect) and "sword" not in inventory:
+            inventory["sword"] = True
+            dialog = "Sword added to inventory"
+            dialog_counter = 30
+            dialog_position = (300, 200)
+
+        if hero_rect.colliderect(potion_rect) and "potion" not in inventory:
+            inventory["potion"] = True
+            dialog = "Potion added to inventory"
+            dialog_counter = 30
+            dialog_position = (300, 200)
+
+        if hero_rect.colliderect(key_rect) and "key" not in inventory:
+            inventory["key"] = True
+            dialog = "Key added to inventory"
+            dialog_counter = 30
+            dialog_position = (300, 200)
+
+        if hero_rect.colliderect(crown_rect) and "crown" not in inventory:
+            inventory["crown"] = True
+            dialog = "Crown added to inventory"
+            dialog_counter = 30
+            dialog_position = (300, 200)
+
+        # Draw the characters
+        screen.blit(snake, snake_rect)
+        pygame.draw.rect(screen, (0, 255, 0), snake_rect, 3)
+
+        # Only draw the gold if it hasn't been picked up
+
+        if "sword" not in inventory:
+            screen.blit(sword, sword_rect)
+        if "potion" not in inventory:
+            screen.blit(potion, potion_rect)
+        if "crown" not in inventory:
+            screen.blit(crown, crown_rect)
+        if "gold" not in inventory and hero_rect.colliderect(snake_rect):
+            screen.blit(treasure, treasure_rect)
+
+        # Pick the sprite frame to draw
+        hero_sprite = hero[frame_count % len(hero)]
+        # Flip the sprite depending on direction
+        if not is_facing_left:
+            hero_sprite = pygame.transform.flip(hero_sprite, True, False)
+        screen.blit(hero_sprite, hero_rect)
+        pygame.draw.rect(screen, (0, 255, 0), hero_rect, 3)
+
+        # draw any dialog
+        if dialog:
+            textsurface = myfont.render(dialog, False, (0, 0, 0))
+            screen.blit(textsurface, dialog_position)
+            # Track how long the dialog is on screen
+            dialog_counter -= 1
+            if dialog_counter == 0:
+                dialog = ''
+
+        # Bring drawn changes to the front
+        pygame.display.update()
+
+        frame_count += 1
+
+        # 30 fps
+        clock.tick(30)
+
+    # loop is over
+    pygame.quit()
+    sys.exit()
+
+
+# Start the program
+main()
